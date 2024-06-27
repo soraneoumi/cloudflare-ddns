73412eaf88d190d4d15be2dd55308d7871779d76
jk <22672990+soraneoumi@users.noreply.github.com>
2024-06-27 16:33:05 +0900

    Add A record updating flow

--- ddns.py
+++ ddns.py
@@ -15,6 +15,15 @@ headers = {
     'Content-Type': 'application/json'
 }
 
+def get_ipv4_address():
+    # Get the public IPv4 address
+    try:
+        result = subprocess.run(['curl', 'inet-ip.info'], capture_output=True, text=True, check=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError:
+        print("Failed to get IPv4 address")
+        return None
+
 def get_ipv6_address():
     # Get the public IPv6 address
     default_iface = os.popen("ip -6 route | grep default | awk '{print $5}'").read().strip()
@@ -30,46 +39,60 @@ def get_zone_id():
     zone_id = data['result'][0]['id']
     return zone_id
 
-def get_record_id(zone_id):
-    url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?name={RECORD_NAME}'
+def get_record_id(zone_id, record_type):
+    url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?type={record_type}&name={RECORD_NAME}'
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
     records = data['result']
     return records[0]['id'] if records else None
 
-def create_record(zone_id, ipv6_address):
+def create_record(zone_id, ip_address, record_type):
     url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
     data = {
-        'type': 'AAAA',
+        'type': record_type,
         'name': RECORD_NAME,
-        'content': ipv6_address,
+        'content': ip_address,
         'ttl': 120
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return json.loads(response.text)['success']
 
-def update_record(zone_id, record_id, ipv6_address):
+def update_record(zone_id, record_id, ip_address, record_type):
     url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}'
     data = {
-        'type': 'AAAA',
+        'type': record_type,
         'name': RECORD_NAME,
-        'content': ipv6_address,
+        'content': ip_address,
         'ttl': 120
     }
     response = requests.put(url, headers=headers, data=json.dumps(data))
     return json.loads(response.text)['success']
 
+def update_dns(ip_address, record_type):
+    zone_id = get_zone_id()
+    record_id = get_record_id(zone_id, record_type)
+
+    if record_id:
+        success = update_record(zone_id, record_id, ip_address, record_type)
+    else:
+        success = create_record(zone_id, ip_address, record_type)
+
+    if success:
+        action = "updated" if record_id else "created"
+        print(f'Successfully {action} {RECORD_NAME} ({record_type}) with {ip_address}')
+    else:
+        print(f'Failed to update or create {record_type} record')
+
+# Update IPv4
+ipv4_address = get_ipv4_address()
+if ipv4_address:
+    update_dns(ipv4_address, 'A')
+else:
+    print("Failed to update IPv4 record: No IPv4 address obtained")
+
+# Update IPv6
 ipv6_address = get_ipv6_address()
-zone_id = get_zone_id()
-record_id = get_record_id(zone_id)
-
-if record_id:
-    success = update_record(zone_id, record_id, ipv6_address)
+if ipv6_address:
+    update_dns(ipv6_address, 'AAAA')
 else:
-    success = create_record(zone_id, ipv6_address)
-
-if success:
-    action = "updated" if record_id else "created"
-    print(f'Successfully {action} {RECORD_NAME} with {ipv6_address}')
-else:
-    print('Failed to update or create record')
+    print("Failed to update IPv6 record: No IPv6 address obtained")
